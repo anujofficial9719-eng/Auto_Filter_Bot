0e937070f3969cf01b9fec7c73ff013038aa5bb8
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-14 17:16:04 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -229,7 +229,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/Anujedits76">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/Anujedits76">[ Anuj Kumar ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
