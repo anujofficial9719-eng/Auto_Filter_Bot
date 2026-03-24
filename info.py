3c8bf5b2b45d8a02d0cd3d773f5bcb4843e152f7
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-25 04:40:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -108,13 +108,13 @@ TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/anujedits76")   # Third tut
 
 # Verification (Must Fill All Veriables. Else You Got Error
 SHORTENER_API = environ.get("SHORTENER_API", "05e572390d5a204da2ae17f59ff2fadfe2aaa23a") # Shortener API key
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "omegalinks.in") # Shortener website
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "ShrinkMe.io") # Shortener website
 
 SHORTENER_API2 = environ.get("SHORTENER_API2", "05e572390d5a204da2ae17f59ff2fadfe2aaa23a")  # Shortener API key for second website
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "omegalinks.in") # Shortener website for second website
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "ShrinkMe.io") # Shortener website for second website
 
 SHORTENER_API3 = environ.get("SHORTENER_API3", "05e572390d5a204da2ae17f59ff2fadfe2aaa23a")  
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "omegalinks.io") # Shortener website for third website
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "ShrinkMe.io") # Shortener website for third website
 
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "1200")) # Time gap for two-step verification in seconds (default: 20 minutes)
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))    
